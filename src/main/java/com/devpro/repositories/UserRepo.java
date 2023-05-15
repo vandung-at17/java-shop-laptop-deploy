7062cfe54b726574859ce549155875d501e69111
vandung-at17 <dungsongoku3@gmail.com>
2023-05-16 00:59:47 +0700

    update /01/00

--- src/main/java/com/devpro/repositories/UserRepo.java
+++ src/main/java/com/devpro/repositories/UserRepo.java
@@ -3,7 +3,6 @@ package com.devpro.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.devpro.entities.Category;
 import com.devpro.entities.User;
 
 
